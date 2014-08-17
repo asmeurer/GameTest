eb27553ad1fc5b0bf06a7eeeb5a74fef2baffd95
Aaron Meurer <asmeurer@gmail.com>
2014-08-16 21:26:26 -0500

    Add another sprite that only moves when touched
    I don't know if there is a better way to do this.

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -20,7 +20,7 @@ class GameScene: SKScene {
         //        self.addChild(myLabel)
         let center = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
 
-        for s in [sprite1, sprite2, sprite3] {
+        for s in [sprite1, sprite2, sprite3, sprite4] {
             s.xScale = 3
             s.yScale = 3
             s.position = center
@@ -28,6 +28,7 @@ class GameScene: SKScene {
 
             physics_body.affectedByGravity = false
             physics_body.allowsRotation = false
+
 
             s.physicsBody = physics_body
             self.addChild(s)
@@ -42,6 +43,9 @@ class GameScene: SKScene {
     let sprite1 = SKSpriteNode(color: UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0), size: CGSize(width: 10, height: 10))
     let sprite2 = SKSpriteNode(color: UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0), size: CGSize(width: 10, height: 10))
     let sprite3 = SKSpriteNode(color: UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0), size: CGSize(width: 10, height: 10))
+    let sprite4 = SKSpriteNode(color: UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0), size: CGSize(width: 10, height: 10))
+
+    var sprite4_selected = false
 
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
@@ -53,6 +57,13 @@ class GameScene: SKScene {
             let action = SKAction.moveTo(location, duration: 1)
             sprite1.runAction(SKAction.repeatAction(action, count: 1))
 
+
+            if CGRectContainsPoint(CGRect(origin: sprite4.position, size: sprite4.size), location) {
+                sprite4_selected = true
+            } else {
+                sprite4_selected = false
+            }
+
         }
     }
 
@@ -61,8 +72,14 @@ class GameScene: SKScene {
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
 
-            let action = SKAction.moveTo(location, duration: 0.1)
-            sprite2.runAction(SKAction.repeatAction(action, count: 1))
+            let action = SKAction.moveTo(location, duration: 0.01)
+
+            // This doesn't really work that well, but it demonstrates how to move sprite4 conditionally. 
+            if sprite4_selected {
+                sprite4.runAction(SKAction.repeatAction(action, count: 1))
+            } else {
+                sprite2.runAction(SKAction.repeatAction(action, count: 1))
+            }
         }
     }
 
