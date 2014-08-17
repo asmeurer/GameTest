dd3a125efc581af2d5c6d0fda4f3678909194540
Aaron Meurer <asmeurer@gmail.com>
2014-08-17 00:39:08 -0500

    Almost working multitouch drags
    It works, except the location that it moves it too is off somehow. Uses
    suggestions from http://eppz.eu/blog/spritekit-drag/.

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Aaron Meurer. All rights reserved.
 //
 
+
 import SpriteKit
 
 class GameScene: SKScene {
@@ -45,55 +46,69 @@ class GameScene: SKScene {
     let sprite3 = SKSpriteNode(color: UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0), size: CGSize(width: 10, height: 10))
     let sprite4 = SKSpriteNode(color: UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0), size: CGSize(width: 10, height: 10))
 
-    var sprite4_selected = false
+    var selected: [UITouch: SKNode] = [:]
 
+    func selectNodeForTouch(touchLocation: CGPoint) {
+        // http://www.raywenderlich.com/44270/sprite-kit-tutorial-how-to-drag-and-drop-sprites
+        let touchedNode = nodeAtPoint(touchLocation)
+
+
+    }
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
 
-
+        selected = [:]
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
 
-            let action = SKAction.moveTo(location, duration: 1)
-            sprite1.runAction(SKAction.repeatAction(action, count: 1))
+//            let action = SKAction.moveTo(location, duration: 1)
+//            sprite1.runAction(SKAction.repeatAction(action, count: 1))
 
+            selected[touch as UITouch] = nodeAtPoint(location)
 
-            if CGRectContainsPoint(CGRect(origin: sprite4.position, size: sprite4.size), location) {
-                sprite4_selected = true
-            } else {
-                sprite4_selected = false
-            }
 
         }
     }
 
-    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
-
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-
-            let action = SKAction.moveTo(location, duration: 0.01)
-
-            // This doesn't really work that well, but it demonstrates how to move sprite4 conditionally. 
-            if sprite4_selected {
-                sprite4.runAction(SKAction.repeatAction(action, count: 1))
-            } else {
-                sprite2.runAction(SKAction.repeatAction(action, count: 1))
-            }
-        }
-    }
+//    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+//
+//        for touch: AnyObject in touches {
+//            let location = touch.locationInNode(self)
+//
+//            let action = SKAction.moveTo(location, duration: 0.01)
+//
+//            // This doesn't really work that well, but it demonstrates how to move sprite4 conditionally. 
+//            for (node, t) in selected {
+////                if t == touch {
+//                    node.runAction(SKAction.repeatAction(action, count: 1))
+////                }
+//            }
+////            if sprite4_selected {
+////                sprite4.runAction(SKAction.repeatAction(action, count: 1))
+////            } else {
+////                sprite2.runAction(SKAction.repeatAction(action, count: 1))
+////            }
+//        }
+//    }
 
     override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
 
-            let action = SKAction.moveTo(location, duration: 1.0)
-            sprite3.runAction(SKAction.repeatAction(action, count: 1))
+            selected[touch as UITouch] = nil
+
+//            let action = SKAction.moveTo(location, duration: 1.0)
+//            sprite3.runAction(SKAction.repeatAction(action, count: 1))
         }
     }
 
 
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        for (touch, node) in selected {
+            let location = touch.locationInView(nil)
+            let action = SKAction.moveTo(location, duration: 0.01)
+            node.runAction(SKAction.repeatAction(action, count: 1))
+        }
     }
 }
